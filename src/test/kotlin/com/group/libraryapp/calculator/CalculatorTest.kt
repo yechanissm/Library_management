1b7452906ea5ea80079c8d574c57d4a21e51228f
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-09-30 17:32:50 +0900

    덧셈, 뺄셈, 곱셈 테스트

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -5,6 +5,8 @@ import java.lang.IllegalStateException
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.multiplyTest()
+    calculatorTest.minusTest()
 }
 
 class CalculatorTest {
@@ -19,6 +21,33 @@ class CalculatorTest {
 
         //then
         if(calculator.number != 8) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun minusTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.minus(3)
+
+        //then
+        if(calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun multiplyTest() {
+
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.multiply(3)
+
+        //then
+        if(calculator.number != 15) {
             throw IllegalStateException()
         }
     }
