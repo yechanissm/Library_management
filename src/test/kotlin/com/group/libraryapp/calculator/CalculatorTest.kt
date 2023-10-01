8829b5c7a55aea55955b871cca7599c0ec6d4fbf
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-10-01 12:58:31 +0900

    나눗셈 테스트 - 수동테스트

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,5 +1,6 @@
 package com.group.libraryapp.calculator
 
+import java.lang.IllegalArgumentException
 import java.lang.IllegalStateException
 
 fun main() {
@@ -7,6 +8,8 @@ fun main() {
     calculatorTest.addTest()
     calculatorTest.multiplyTest()
     calculatorTest.minusTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 class CalculatorTest {
@@ -50,5 +53,37 @@ class CalculatorTest {
         if(calculator.number != 15) {
             throw IllegalStateException()
         }
+    }
+
+    fun divideTest() {
+
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.divide(2)
+
+        //then
+        if(calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun divideExceptionTest() {
+
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch(e: IllegalArgumentException) {
+            if( e.message != "0으로 나눌 수 없습니다") {
+                throw IllegalStateException("메시지가 다릅니다")
+            }
+            return
+        } catch(e : Exception) {
+            throw IllegalStateException()
+        }
+        throw IllegalStateException("기대하는 예외가 발생하지 않았습니다.")
     }
 }
