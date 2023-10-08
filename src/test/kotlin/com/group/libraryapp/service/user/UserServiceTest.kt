318e584a84548f4b1e3b7e0303a4743956814b68
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-10-08 12:17:36 +0900

    Junit5으로 Spring Boot 테스트

---
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -0,0 +1,32 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceTest(
+        @Autowired private val userRepository: UserRepository,
+        @Autowired private val userService: UserService,
+) {
+
+    @Test
+    fun saveUserTest() {
+        //given
+        val request = UserCreateRequest("kim", null)
+
+        //when
+        userService.saveUser(request)
+
+        //then
+        val result = userRepository.findAll()
+        assertThat(result).hasSize(1)
+        assertThat(result[0].name).isEqualTo("kim")
+        assertThat(result[0].age).isNull()
+    }
+
+}
