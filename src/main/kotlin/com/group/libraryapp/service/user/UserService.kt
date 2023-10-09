28337574d45b33f7c03cfec9577d3eaab17b90c3
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-10-09 19:38:49 +0900

    DTO를 Kotlin으로 변경

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -26,7 +26,7 @@ class UserService(
     @Transactional(readOnly = true)
     fun getUsers(): List<UserResponse> {
         return userRepository.findAll().
-        map { user-> UserResponse(user) }
+        map { user-> UserResponse.of(user) }
     }
 
     @Transactional
@@ -38,6 +38,7 @@ class UserService(
     @Transactional
     fun deleteUser(name: String) {
         val user = userRepository.findByName(name) ?: fail()
+        userRepository.delete(user)
     }
 
 
