af3d06df65bed6719511dde644545bcf8fd3abd2
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-10-11 12:39:36 +0900

    Querydsl 빈 등록 과정

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.util.Optional
 
-interface UserRepository : JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
 
     fun findByName(name: String) : User?
 
---
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepositoryCustom.kt
@@ -0,0 +1,4 @@
+package com.group.libraryapp.domain.user
+
+interface UserRepositoryCustom {
+}
