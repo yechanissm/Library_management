910454b9d4ee7b68460606747fa04efd1a606b24
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-10-11 12:14:56 +0900

    QueryDSL 환경변수 설정

--- build/tmp/kapt3/stubs/main/com/group/libraryapp/calculator/Calculator.java
+++ build/tmp/kapt3/stubs/main/com/group/libraryapp/calculator/Calculator.java
@@ -2,19 +2,16 @@ package com.group.libraryapp.calculator;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0002\b\u0005\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u000e\u0010\b\u001a\u00020\t2\u0006\u0010\n\u001a\u00020\u0003J\u000e\u0010\u000b\u001a\u00020\t2\u0006\u0010\n\u001a\u00020\u0003J\u000e\u0010\f\u001a\u00020\t2\u0006\u0010\n\u001a\u00020\u0003J\u000e\u0010\r\u001a\u00020\t2\u0006\u0010\n\u001a\u00020\u0003R\u001a\u0010\u0002\u001a\u00020\u0003X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\u0004\u00a8\u0006\u000e"}, d2 = {"Lcom/group/libraryapp/calculator/Calculator;", "", "number", "", "(I)V", "getNumber", "()I", "setNumber", "add", "", "operand", "divide", "minus", "multiply", "library-app"})
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0002\b\u0005\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u000e\u0010\b\u001a\u00020\t2\u0006\u0010\n\u001a\u00020\u0003J\u000e\u0010\u000b\u001a\u00020\t2\u0006\u0010\n\u001a\u00020\u0003J\u000e\u0010\f\u001a\u00020\t2\u0006\u0010\n\u001a\u00020\u0003J\u000e\u0010\r\u001a\u00020\t2\u0006\u0010\n\u001a\u00020\u0003R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0011\u0010\u0005\u001a\u00020\u00038F\u00a2\u0006\u0006\u001a\u0004\b\u0006\u0010\u0007\u00a8\u0006\u000e"}, d2 = {"Lcom/group/libraryapp/calculator/Calculator;", "", "_number", "", "(I)V", "number", "getNumber", "()I", "add", "", "operand", "divide", "minus", "multiply", "library-app"})
 public final class Calculator {
-    private int number;
+    private int _number;
     
-    public Calculator(int number) {
+    public Calculator(int _number) {
         super();
     }
     
     public final int getNumber() {
         return 0;
-    }
-    
-    public final void setNumber(int p0) {
     }
     
     public final void add(int operand) {
